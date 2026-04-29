922ab2045debeeb4e647b2e76a58b70f13b1d1c5
ArjnCrly <arjuna.carly1304@gmail.com>
2026-04-30 00:28:12 +0700

    Praktikum 2.1: Retrieving Single Models - find, firstOrFail, firstWhere, findOr

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'Manager_tiga',
-            'nama' => 'Manager 3    ',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
-       
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
