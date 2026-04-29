0130b9bb89f4f40798f22b7a416fb4a8f6184130
ArjnCrly <arjuna.carly1304@gmail.com>
2026-04-30 00:56:44 +0700

    Praktikum 2.5: Attribute Changes - isDirty, isClean, wasChanged

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,15 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-        [
-            'username' => 'manager33',
-            'nama'     => 'Manager Tiga Tiga',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ],
-    );
+        $user = UserModel::create([
+        'username' => 'manager11',
+        'nama'     => 'Manager11',
+        'password' => Hash::make('12345'),
+        'level_id' => 2,
+    ]);
+
+    $user->username = 'manager12';
+
     $user->save();
-    return view('user', ['data' => $user]);
+
+    $user->wasChanged();                      // true  → ada yang berubah
+    $user->wasChanged('username');            // true  → username berubah
+    $user->wasChanged(['username', 'level_id']); // true  → salah satu berubah
+    $user->wasChanged('nama');                // false → nama tidak berubah
+    $user->wasChanged(['nama', 'username']);  // true  → salah satu berubah
+
+    dd($user->wasChanged(['nama', 'username']));
     }
 }
