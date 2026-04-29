490d2b609a8587fd3b052782e3676eed65409fbd
ArjnCrly <arjuna.carly1304@gmail.com>
2026-04-29 17:27:53 +0700

    Praktikum 6: Implementasi Eloquent ORM pada UserModel dan UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function show($id, $name) {  
-        return view('user.profile', [
-            'user_id' => $id, 
-            'user_name' => $name
-        ]);
+    public function index()
+    {
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+       
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
